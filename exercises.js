7d0098d4645fef8bf16377d50633511cdbf08a85
Luke Fiorio <lukefiorio@Lukes-MBP.home>
2019-03-20 20:30:29 -1000

    framework built out

--- exercises.js
+++ exercises.js
@@ -16,3 +16,46 @@
 // 10.generates a list of scores that are odd numbered
 
 
+var secretModule = (function() {
+
+    var scores = [65,76,100,88,17,55];
+
+    function add(a,b) { return a+b };
+    function top(a,b) { return Math.max(a,b) };
+    function low(a,b) { return Math.min(a,b) };
+
+    function sumScores() {
+        return scores.reduce(add);
+    }
+
+    function avgScore() {
+        return scores.reduce(add) / scores.length;
+    }
+
+    function highScore() {
+        return scores.reduce(top);
+    }
+
+    function lowScore() {
+        return scores.reduce(low);
+    }
+
+    function randScore() {
+        
+    }
+
+    return {
+        sumTotal: sumScores,
+        meanScore: avgScore,
+        topScore: highScore,
+        botScore: lowScore
+    }
+
+
+}())
+
+
+console.log(secretModule.sumTotal());
+console.log(secretModule.meanScore());
+console.log(secretModule.topScore());
+console.log(secretModule.botScore());
